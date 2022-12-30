ef6452858df4d418f175481120185bcb39063fc4
Agustín De Sousa Mariano <agus.desousa99@gmail.com>
2022-12-30 10:38:13 -0300

    Agregado enunciado y diagrama de clases realizado

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import ejercicio3.interfazDeUsuario.InterfazDeUsuario3;
 
 public class Main {
     public static void main(String[] args) {
-        InterfazDeUsuario1 interfazDeUsuario1 = new InterfazDeUsuario1(); // Interfaz de usuario del ejercicio 1
+        // InterfazDeUsuario1 interfazDeUsuario1 = new InterfazDeUsuario1(); // Interfaz de usuario del ejercicio 1
         InterfazDeUsuario3 interfazDeUsuario3 = new InterfazDeUsuario3(); // Interfaz de usuario del ejercicio 3
     }
 }
